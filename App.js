a73621befe7b35ab06d0ff386b1d0b3a64c4be7e
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-03-20 18:31:54 +0100

    swagger start

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-resolved.json");
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Зробити папку з файлами доступною
+app.use(
+  "/favicon.ico",
+  express.static(path.join(__dirname, "public", "favicon.ico"))
+);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the API");
 });
+
+app.get("/test", (req, res) => {
+  res.send("This is a test route");
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
