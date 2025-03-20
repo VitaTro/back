65963835eb840740b4c258f7abebf2536c2551f8
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-03-20 15:56:13 +0100

    start

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = require("./App");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
