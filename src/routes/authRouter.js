def91dde888efd9fbd796ffeb9edfac24c72630c
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-03-30 10:21:48 +0200

    start passport

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -7,12 +7,26 @@ const {
   register,
   registerAdmin,
 } = require("../controllers/authController");
+const { check, validationResult } = require("express-validator");
+const authMiddleware = require("../middleware/userMiddleware");
 router.get("/check-admin", checkFirstAdmin);
-
-router.post("/register/user", (req, res, next) => {
-  req.body.role = "user";
-  register(req, res, next);
-});
+router.post(
+  "/register/user",
+  [
+    check("email", "Введіть коректний email").isEmail(),
+    check("password", "Пароль має бути щонайменше 6 символів").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    req.body.role = "user";
+    register(req, res, next);
+  }
+);
 
 router.post("/register/admin", (req, res, next) => {
   req.body.role = "admin";
@@ -23,5 +37,8 @@ router.post("/register/admin", registerAdmin);
 router.post("/login", login);
 
 router.post("/logout", logout);
+router.post("/protected-route", authMiddleware, (req, res) => {
+  res.send("Це захищений ресурс.");
+});
 
 module.exports = router;
