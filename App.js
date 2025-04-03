252f4faf9f9f983114f799a5ce76169972f94181
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-04-03 17:28:52 +0200

    спроба1

--- App.js
+++ App.js
@@ -3,21 +3,18 @@ const cors = require("cors");
 const path = require("path");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
-const upload = require("./src/middleware/uploadMiddleware");
 const { cloudinary, storage } = require("./src/config/cloudinary");
+const upload = require("./src/middleware/uploadMiddleware");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Зробити папку доступною для статичних файлів
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Зробити папку з файлами доступною
 app.use(
   "/favicon.ico",
   express.static(path.join(__dirname, "public", "favicon.ico"))
 );
 
-// Основний маршрут
 app.get("/", (req, res) => {
   res.send("Welcome to the API");
 });
@@ -26,10 +23,7 @@ app.get("/test", (req, res) => {
   res.send("This is a test route");
 });
 
-// Swagger документація
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Маршрут для завантаження зображень
 app.post("/upload", upload.single("photo"), (req, res) => {
   cloudinary.uploader.upload(req.file.path, (error, result) => {
     if (error) {
@@ -39,5 +33,4 @@ app.post("/upload", upload.single("photo"), (req, res) => {
     }
   });
 });
-
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("./config");
+const dotenv = require("dotenv");
 const app = require("./App");
 const { cloudinary } = require("./src/config/cloudinary");
-
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 mongoose
