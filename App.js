d7bd94bc0086f15d4f08a850fe7f9933a9ad4cc6
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-04-03 17:13:25 +0200

    спроба

--- App.js
+++ App.js
@@ -4,33 +4,11 @@ const path = require("path");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const upload = require("./src/middleware/uploadMiddleware");
-const { cloudinary } = require("./src/config/cloudinary");
-const ProductRouter = require("./src/routes/productRouter");
-const AuthRouter = require("./src/routes/authRouter");
-const UserRouter = require("./src/routes/userRouter");
+const { cloudinary, storage } = require("./src/config/cloudinary");
 
 const app = express();
 
-// Дозволені походження
-const allowedOrigins = [
-  "https://nika-gold.netlify.app",
-  "http://localhost:3000",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log("CORS Origin:", origin); // Логування для діагностики
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // Дозволити запит
-      } else {
-        callback(new Error("Not allowed by CORS")); // Блокування
-      }
-    },
-    credentials: true, // Дозвіл на передачу кукі
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Зробити папку доступною для статичних файлів
 
@@ -48,11 +26,6 @@ app.get("/test", (req, res) => {
   res.send("This is a test route");
 });
 
-// Основні маршрути
-app.use("/api/products", ProductRouter);
-app.use("/api/auth", AuthRouter);
-app.use("/api/user", UserRouter);
-
 // Swagger документація
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -65,15 +38,6 @@ app.post("/upload", upload.single("photo"), (req, res) => {
       res.json({ url: result.secure_url });
     }
   });
-});
-
-// Обробка помилок CORS
-app.use((err, req, res, next) => {
-  if (err.message === "Not allowed by CORS") {
-    res.status(403).json({ error: "CORS policy blocked the request" });
-  } else {
-    next();
-  }
 });
 
 module.exports = app;
