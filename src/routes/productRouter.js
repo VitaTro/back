4d568923bb16945edc0e1548ecd4f61d9dcdc632
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-04-05 14:49:12 +0200

    clo1

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -38,11 +38,9 @@ router.post("/", async (req, res) => {
       visible,
       createdAt,
     } = req.body;
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-    const result = await cloudinary.uploader.upload(req.file.path);
-    const photoUrl = result.secure_url;
+
+    // const result = await cloudinary.uploader.upload(req.file.path);
+    // const photoUrl = result.secure_url;
 
     const newProduct = new Product({
       name,
