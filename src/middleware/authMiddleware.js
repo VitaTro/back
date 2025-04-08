235e4651b72fe4af526ecf2445c6b4deda3d9b7d
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-04-08 19:32:33 +0200

    unlock

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,38 +1,44 @@
 const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+  // const authHeader = req.headers.authorization;
+  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  //   return res.status(401).json({ message: "Unauthorized" });
+  // }
 
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Додаємо дані користувача до req.user
-    next();
-  } catch (error) {
-    res.status(403).json({ message: "Invalid token" });
-  }
+  // const token = authHeader.split(" ")[1];
+  // try {
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //   req.user = decoded; // Додаємо дані користувача до req.user
+  //   next();
+  // } catch (error) {
+  //   res.status(403).json({ message: "Invalid token" });
+  // }
+  console.log("Skipping authenticateJWT for testing...");
+  // Тимчасово пропускаємо запит
+  next();
 };
 const isAuthenticated = async (req, res, next) => {
-  try {
-    const token = req.header("Authorization")?.replace("Bearer ", ""); // Отримуємо токен із заголовків
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "No token provided. Access denied." });
-    }
+  // try {
+  //   const token = req.header("Authorization")?.replace("Bearer ", ""); // Отримуємо токен із заголовків
+  //   if (!token) {
+  //     return res
+  //       .status(401)
+  //       .json({ message: "No token provided. Access denied." });
+  //   }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Розшифровуємо токен
-    req.user = await User.findById(decoded.id).select("-password"); // Додаємо користувача до req
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET); // Розшифровуємо токен
+  //   req.user = await User.findById(decoded.id).select("-password"); // Додаємо користувача до req
 
-    if (!req.user) {
-      return res.status(404).json({ message: "User not found." });
-    }
+  //   if (!req.user) {
+  //     return res.status(404).json({ message: "User not found." });
+  //   }
 
-    next(); // Продовжуємо роботу
-  } catch (error) {
-    console.error("Authentication error:", error);
-    res.status(401).json({ message: "Invalid or expired token." });
-  }
+  //   next(); // Продовжуємо роботу
+  // } catch (error) {
+  //   console.error("Authentication error:", error);
+  //   res.status(401).json({ message: "Invalid or expired token." });
+  // }
+  console.log("Skipping isAuthenticated for testing...");
+  // Тимчасово пропускаємо запит
+  next();
 };
 module.exports = { authenticateJWT, isAuthenticated };
