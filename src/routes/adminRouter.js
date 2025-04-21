a377c278080912eca01705a56e6556bf73bbd6a4
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-04-21 17:54:31 +0200

    admin get.products

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -29,12 +29,23 @@ router.delete("/users/:id", async (req, res) => {
   }
 });
 
+router.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch products" });
+  }
+});
+
 // Маршрут для створення нового продукту
 router.post("/products", async (req, res) => {
   try {
     const product = new Product(req.body);
     await product.save();
-    res.status(201).json({ message: "Product created", product });
+    res
+      .status(201)
+      .json({ id: product._id, message: "Product added successfully" });
   } catch (error) {
     res.status(500).json({ error: "Failed to create product" });
   }
