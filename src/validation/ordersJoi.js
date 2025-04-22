870742cb1890c18651c9287b9e5122b58bf0ddcd
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-04-22 15:09:10 +0200

    + deliveryAddress

--- src/validation/ordersJoi.js
+++ src/validation/ordersJoi.js
@@ -7,7 +7,16 @@ const orderValidationSchema = Joi.object({
   totalPrice: Joi.number().required(),
   paymentStatus: Joi.string().valid("paid", "unpaid").required(),
   paymentMethod: Joi.string().valid("cash", "card").required(),
-  deliveryAddress: Joi.string().required(),
+  deliveryType: Joi.string().valid("courier", "smartbox", "pickup").required(), // Додаємо тип доставки
+  smartboxDetails: Joi.object({
+    boxId: Joi.string().required(),
+    location: Joi.string().required(),
+  }).optional(),
+  deliveryAddress: Joi.string().when("deliveryType", {
+    is: "courier",
+    then: Joi.required(),
+    otherwise: Joi.optional(), // Адреса потрібна тільки для кур'єра
+  }),
   notes: Joi.string().optional(),
 });
 
