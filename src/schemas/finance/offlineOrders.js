ca42789a5b0979cca1c2d360bd3601976aa7c980
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-04-28 21:10:22 +0200

    Joi

--- src/schemas/finance/offlineOrders.js
+++ src/schemas/finance/offlineOrders.js
@@ -10,6 +10,7 @@ const offlineOrderSchema = new mongoose.Schema(
           ref: "Product",
           required: true,
         },
+        photoUrl: { type: String, required: true },
         quantity: { type: Number, required: true },
         name: { type: String, required: true },
         price: { type: Number, required: true },
--- src/validation/onlineOrdersJoi.js
+++ src/validation/onlineOrdersJoi.js
@@ -1,13 +1,22 @@
 const Joi = require("joi");
 
 const onlineOrderValidationSchema = Joi.object({
-  productId: Joi.string().required(),
+  products: Joi.array()
+    .items(
+      Joi.object({
+        productId: Joi.string().required(),
+        quantity: Joi.number().min(1).required(),
+        name: Joi.string().required(), // Додаємо name
+        price: Joi.number().required(), // Додаємо price
+      })
+    )
+    .required(),
   userId: Joi.string().required(),
   status: Joi.string().valid("new", "completed", "cancelled").required(),
   totalPrice: Joi.number().required(),
   paymentStatus: Joi.string().valid("paid", "unpaid").required(),
   paymentMethod: Joi.string().valid("cash", "card").required(),
-  deliveryType: Joi.string().valid("courier", "smartbox", "pickup").required(), // Додаємо тип доставки
+  deliveryType: Joi.string().valid("courier", "smartbox", "pickup").required(),
   smartboxDetails: Joi.object({
     boxId: Joi.string().required(),
     location: Joi.string().required(),
@@ -15,7 +24,7 @@ const onlineOrderValidationSchema = Joi.object({
   deliveryAddress: Joi.string().when("deliveryType", {
     is: "courier",
     then: Joi.required(),
-    otherwise: Joi.optional(), // Адреса потрібна тільки для кур'єра
+    otherwise: Joi.optional(),
   }),
   notes: Joi.string().optional(),
 });
