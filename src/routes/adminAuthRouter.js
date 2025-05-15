409efae2a96fcf4048ab21a0406785f5c7bac6a8
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-05-15 17:26:31 +0200

    upd adminAuthRouter

--- src/routes/adminAuthRouter.js
+++ src/routes/adminAuthRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const sendEmail = require("../../emailService");
+const { sendEmail } = require("../../emailService");
 const User = require("../schemas/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
