7b9082cd8b5374693b8faada398859bf2f123cc5
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-05-20 21:28:04 +0200

    cors

--- App.js
+++ App.js
@@ -40,6 +40,20 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    allowedOrigins.includes(req.headers.origin)
+      ? req.headers.origin
+      : "https://nika-gold.netlify.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Зробити папку з файлами доступною
